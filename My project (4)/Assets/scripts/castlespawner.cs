0188684f8e4221dde377b1af4ab092cee9ca5ed6
emasudaualberta <emasuda@ualberta.ca>
2025-07-19 15:55:04 -0600

    skeleton

---
+++ My project (4)/Assets/scripts/castlespawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class castlespawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public enemyWave enemyWave;
+    public GameObject enemyOne;
+    public GameObject enemyTwo;
+    public float gameSpeed;
+    public string wave= "1..1..1..111";
+    void Awake()
+    {
+        gameSpeed = gameManager.Instance.gamespeed;
+        StartCoroutine(ReadEnemyWave());
+    }
+
+    IEnumerator ReadEnemyWave()
+    {
+        yield return new WaitForSecondsRealtime(gameSpeed);
+
+        foreach(char action in wave)
+        {
+            if (action == '.')
+            {
+                yield return new WaitForSecondsRealtime(gameSpeed);
+            }
+            else
+            {
+                instantiateEnemy(action);
+            }
+        }
+        Debug.Log("end");
+
+    }
+
+
+    void instantiateEnemy(char enemy)
+    {
+        if (enemy == '1')
+        {
+           Instantiate(enemyOne);
+        }
+        else if (enemy == '2')
+        {
+            Instantiate(enemyTwo);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ My project (4)/Assets/scripts/gameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float gamespeed = 0.5f; //time taken to complete a spawn action
+    void Start()
+    {
+        
+    }
+
+    private static gameManager _instance;
+
+    public static gameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<gameManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
