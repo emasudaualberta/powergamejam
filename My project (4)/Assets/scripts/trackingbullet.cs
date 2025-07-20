2c817a3e5dd8c8b43ab837e73b8fe15038225cc7
emasudaualberta <emasuda@ualberta.ca>
2025-07-19 21:51:35 -0600

    bulelt

--- My project (4)/Assets/scripts/trackingbullet.cs
+++ My project (4)/Assets/scripts/trackingbullet.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class trackingbullet : MonoBehaviour
 {
+
+    GameObject trackingObject;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    public void setTarget(GameObject target)
+    {
+        trackingObject = target;
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (trackingObject != null) {
+
+        }
+        Debug.Log("asdfa");
+        transform.position += Vector3.forward * Time.deltaTime;
     }
 }
