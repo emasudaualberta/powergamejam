55831a964210519af226f75a7342cae2268e7e64
bowlosoup <72242375+bowlosoup@users.noreply.github.com>
2025-07-19 21:38:03 -0600

    ad

--- My project (4)/Assets/scripts/Wizard.cs
+++ My project (4)/Assets/scripts/Wizard.cs
@@ -8,6 +8,8 @@ public class Wizard : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 movement;
 
+    public GameObject BombProjectile;
+    public float projectileSpeed = 6f;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -20,21 +22,39 @@ public class Wizard : MonoBehaviour
     void Movement()
     {
         // Get WASD input
-        movement.x = Input.GetAxisRaw("Horizontal"); // A/D or Left/Right
-        movement.y = Input.GetAxisRaw("Vertical");   // W/S or Up/Down
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical"); 
         if (movement.x > 0)
             transform.localScale = new Vector3(3, 3, 1);
         else if (movement.x < 0)
             transform.localScale = new Vector3(-3, 3, 1);
     }
+    void Update()
+    {
+        Movement();
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            InstantiateBomb();
+        }
+    }
+    void InstantiateBomb()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
 
+        Vector2 direction = (mousePos - transform.position).normalized;
+    
+        GameObject Projectile = Instantiate(BombProjectile, transform.position, Quaternion.identity);
+        Rigidbody2D rbProjectile = Projectile.GetComponent<Rigidbody2D>();
+        if (rbProjectile != null)
+        {
+            rbProjectile.velocity = direction * projectileSpeed;
+        }
+    }
     void FixedUpdate()
     {
         // Apply movement
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
-    void Shoot()
-    {
 
-    }
 }
