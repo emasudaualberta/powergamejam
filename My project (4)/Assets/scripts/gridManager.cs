d1869f9729db603f02664dfdc5f190fee619c6f8
ncwong26 <nathancwong26@gmail.com>
2025-07-19 19:16:08 -0600

    initial commit

---
+++ My project (4)/Assets/scripts/gridManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridManager : MonoBehaviour
+{
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+
+    [SerializeField] private Tile _tile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        for(i = 0; i < width; i++)
+        {
+            for(j = 0; i < width; j++)
+            {
+                var spawnedTile = Instantiate(_tile, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"tile {x} {y}";
+            }
+        }
+    }
+}
